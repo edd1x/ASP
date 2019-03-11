89fd0a5aae98c56b31de82efab6f8d67f3341a1e
edd1x <46432589+edd1x@users.noreply.github.com>
2019-03-11 15:52:15 +0100

    Create PrioritetniRed.h

---
+++ Prioritetni Red/PrioritetniRed.h
@@ -0,0 +1,20 @@
+//PRIORITETNI RED
+#pragma once
+
+#include "Cvor.h"
+
+template <typename Tip>
+class PrioritetniRed
+{
+
+public:
+
+	virtual void add(Tip v) = 0;
+
+	virtual Tip ukloni() = 0;
+
+	virtual bool isPrazan() = 0;
+
+	virtual void print(string pre = "", string post = ", ") = 0;
+};
+
