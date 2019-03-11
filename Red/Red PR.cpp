c17232aaee124162d77d243da0e6bccded935ec5
edd1x <46432589+edd1x@users.noreply.github.com>
2019-03-11 15:51:56 +0100

    Create Red PR.cpp

---
+++ Red/Red PR.cpp
@@ -0,0 +1,78 @@
+//RED POVEZANA REPREZENTACIJA
+#include<iostream>
+#include "Cvor.h"
+template <class Tip>
+class RedPov
+{
+private:
+	Cvor<Tip>* pocetak;
+	Cvor<Tip>* kraj;
+public:
+	RedPov()
+	{
+		pocetak = nullptr;
+		kraj = nullptr;
+	}
+	void dodaj(Tip v)
+	{
+		Cvor<Tip>* t = new Cvor<Tip>(v, nullptr);
+		if (!isPrazan())
+		{
+			kraj->next = t;
+			kraj = t;
+		}
+		else
+		{
+			kraj = t;
+			pocetak = t;
+		}
+
+	}
+
+	Tip ukloni()
+	{
+		if (isPrazan())
+			throw exception("Greska. Nije moguce ukloniti elemenat iz praznog reda.");
+
+		Cvor<Tip>* t = pocetak;
+		pocetak = pocetak->next;
+
+		Tip x = t->info;
+		delete t;
+
+
+		if (pocetak == nullptr)
+			kraj = nullptr;
+
+		return x;
+
+	}
+
+	bool isPrazan()
+	{
+		return pocetak == nullptr;
+	}
+
+	void print(string pre = "", string post = ", ")
+	{
+		Cvor<Tip>* t = pocetak;
+		while (t != nullptr)
+		{
+			cout << pre << t->info << post;
+			t = t->next;
+		}
+	}
+
+	bool sadrzi(Tip v)
+	{
+		Cvor<Tip>* t = pocetak;
+		while (t != nullptr)
+		{
+			if (IsJednako(t->info, v))
+				return true;
+			t = t->next;
+		}
+		return false;
+	}
+};
+
